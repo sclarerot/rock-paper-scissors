f6ed518d34cdf30a1cd9ea7571ffe1afc1ae8c4b
sclarerot <sclarerot@proton.me>
2025-02-27 14:02:09 -0500

    Fixed capitalization.
    I changed the strings to lowercase. Ugh..

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let callRandom = Math.floor(Math.random() * 3)
 
 function getComputerChoice(computerPlay) {
   if(computerPlay == 0) {
-    return "Rock"
+    return "rock"
   }
   else if(computerPlay == 1) {
-    return "Paper"
+    return "paper"
   }
   else if(computerPlay == 2) {
-    return "Scissors"
+    return "scissors"
   }
 }
