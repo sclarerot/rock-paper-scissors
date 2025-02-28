4c448bb1a40ea9d239b3e7db97a52db6e76151e8
sclarerot <sclarerot@proton.me>
2025-02-28 10:22:26 -0500

    Create player choice function and score variables.
    I added a function that takes in a string prompt and converts it to lowercase, as well as initializing the score variables.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let callRandom = Math.floor(Math.random() * 3)
+let playerChoice = prompt("Please enter rock, paper, or scissors")
+playerChoice = playerChoice.toLowerCase()
 
 function getComputerChoice(computerPlay) {
   if(computerPlay == 0) {
     return "rock"
-  }
-  else if(computerPlay == 1) {
+  }  else if(computerPlay == 1) {
     return "paper"
-  }
-  else if(computerPlay == 2) {
+  }  else if(computerPlay == 2) {
     return "scissors"
   }
 }
+
+function getHumanChoice(humanPlay) {
+    if(humanPlay == "rock") {
+      return "rock";
+    } else if(humanPlay == "paper") {
+      return "paper";
+    } else if(humanPlay == "scissors") {
+      return "scissors";
+    } else {
+      return "Invalid selection! Please try again.";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
